ce5ed63e9924bf49b05b4113e33a1182ac589100
YtzTrue <yuretztrue@gmail.com>
2022-12-26 18:59:04 +0300

    BaseButton component created, classic button props added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,83 @@
-<script setup>
+<script>
+import BaseButtonVue from './components/BaseButton.vue'
 
+export default {
+  name: 'app',
+  components: { BaseButtonVue },
+  data() {
+    return {}
+  },
+  methods: {
+    clickBtn() {
+      console.log('click')
+    }
+  }
+}
 </script>
 
 <template>
   <div class="container">
-    <h1>This is vue3-button-component</h1>
-    <div class="content"></div>
+    <h1>This is Vue3-button-component</h1>
+    <div class="content">
+      <div class="content__wrapper">
+        <h2 class="content__title">(button-classic)</h2>
+        <h2 class="content__btn-title">(primary)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="primary" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(secondary)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="secondary" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(warning)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="warning" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(disabled)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="disabled" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(info)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="info" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(danger)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="danger" @click.prevent="clickBtn"/>
+        <h2 class="content__btn-title">(action)</h2>
+        <BaseButtonVue class="content__btn" label="Отправить письмо" color="action" @click.prevent="clickBtn"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-@import "@/styles/global.scss"
+@import "@/styles/global.scss";
+
+#app {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+  font-family: Phoshate, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+}
+
+.content {
+  display: flex;
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+  &__title {
+    align-self: center;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+  }
+  &__btn-title {
+    margin-top: 0;
+    margin-bottom: 8px;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 24px;
+    color: #fff;
+  }
+  &__btn {
+    margin-bottom: 24px;
+  }
+}
 </style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: 'Button'
+        },
+        color: {
+            type: String,
+            default: 'primary'
+        },
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        click() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
+
+<template>
+    <div 
+    class="button"
+    :class="[`button_${color}`]">
+        <span>
+            {{ label }}
+        </span>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .button {
+        padding: 12px 46px;
+        height: 52px;
+        border-radius: 20px;
+        border: none;
+        outline: none;
+        font-family: Phoshate, Helvetica, Arial, sans-serif;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 24px;
+        color: #fff;
+        cursor: pointer;
+
+        span {
+            vertical-align: middle;
+        }
+        &_primary {
+            background-color: var(--primary);
+        }
+        &_secondary {
+            background-color: var(--secondary);
+        }
+        &_warning {
+            background-color: var(--warning);
+        }
+        &_disabled {
+            background-color: var(--disabled);
+            color: #767679;
+            pointer-events: none;
+        }
+        &_info {
+            background-color: var(--info);
+        }
+        &_danger {
+            background-color: var(--danger);
+        }
+        &_action {
+            background-color: var(--action);
+        }
+    }
+</style>
