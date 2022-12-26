4b0839cb09fc1ef1d218c7e3c211aea1b7c1f865
YtzTrue <yuretztrue@gmail.com>
2022-12-26 16:00:57 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>This is vue3-button-component</h1>
+    <div class="content"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/styles/global.scss"
+</style>
